17d4f3f43c7f28d62f5b6545084d2f91687aae47
Sho Fujiwara <shofujiwara505@gmail.com>
2023-07-04 13:47:23 +0900

    picked an image

--- src/comp/Home.jsx
+++ src/comp/Home.jsx
@@ -1,7 +1,15 @@
+import heroImg from "../assets/imgs/smallVan.jpg";
+
 export default function Home() {
   return (
-    <div>
-      <h1>HOME PAGEEE</h1>
+    <div className="font-inter flex ">
+      <img src={heroImg} alt="small van" className="absolute top-0 -z-10 h-full w-full brightness-75 object-cover " />
+      <div className="mx-auto">
+        <h1 className="text-6xl text-white ">
+          Got travel <strong className="underline">PLANS</strong>?<br /> We got travel{" "}
+          <strong className="underline">VANS</strong>
+        </h1>
+      </div>
     </div>
   );
 }
