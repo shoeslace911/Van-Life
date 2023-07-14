264337b571455ef12c7f89c72c459f939b478c84
Sho Fujiwara <shofujiwara505@gmail.com>
2023-07-14 14:15:53 +0900

    rendered the vans, now for desig

--- src/comp/Vans.jsx
+++ src/comp/Vans.jsx
@@ -22,19 +22,21 @@ export default function Vans() {
 
   console.log(vanList);
   return (
-    <div className="text-black pt-24 ">
+    <div className="text-black pt-24 px-12 py-8">
       <h1 className="text-5xl text-center font-inter">Vans</h1>
-
-      {vanList.vans.map((van) => {
-        return (
-          <div key={van.id}>
-            <h1 className="font-inter">{van.name}</h1>
-            <h2>{van.type}</h2>
-            <p className="text-lg">{van.description}</p>
-            <img src={van.imageUrl} className="w-32" alt="Van image" />
-          </div>
-        );
-      })}
+      <div className="flex flex-wrap justify-evenly">
+        {vanList.vans.map((van) => {
+          return (
+            <div key={van.id} className="w-72 h-fit">
+              <h1 className="font-inter">{van.name}</h1>
+              <h2>{van.type}</h2>
+              <p>${van.price}/day</p>
+              <p className="text-lg">{van.description}</p>
+              <img src={van.imageUrl} alt="Van image" />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
